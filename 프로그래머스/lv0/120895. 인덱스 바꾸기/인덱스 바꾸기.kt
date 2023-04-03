2d453725e98cd2065d41d87151d2a0a853ed1466
ANSHyeon <127817240+ANSHyeon@users.noreply.github.com>
2023-04-03 23:33:22 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 15.01 ms, Memory: 63 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(my_string: String, num1: Int, num2: Int): String {
+        var array = my_string.toCharArray()
+        var temp = array[num1]
+        array[num1] = array[num2]
+        array[num2] = temp
+        return array.joinToString("")
+    }
+}
