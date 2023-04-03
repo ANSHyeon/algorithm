ab73b7951bed736f59dde10312f368ff559eb79f
ANSHyeon <127817240+ANSHyeon@users.noreply.github.com>
2023-04-03 19:30:26 +0900

    [level 0] Title: 가위 바위 보, Time: 7.86 ms, Memory: 61.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(rsp: String) = rsp.replace("2","muk")
+    .replace("0","bba")
+    .replace("5","jji")
+    .replace("muk","0") 
+    .replace("jji","2")  
+    .replace("bba","5")
+}
