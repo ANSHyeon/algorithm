a9cedb10c3bab64c701d936e38962b54c9f2532a
ANSHyeon <127817240+ANSHyeon@users.noreply.github.com>
2023-04-03 16:58:05 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 5.63 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.kt
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.kt
@@ -1,10 +1,3 @@
 class Solution {
-    fun solution(my_string: String): Int{
-        var str = my_string.filter{it in ('1'..'9')}
-        var sum = 0
-        for(i in str){
-            sum = sum + i.toString().toInt()
-        }
-        return sum
-    }
+    fun solution(my_string: String) = my_string.filter{it in ('1'..'9')}.map{it.toString().toInt()}.sum()
 }
