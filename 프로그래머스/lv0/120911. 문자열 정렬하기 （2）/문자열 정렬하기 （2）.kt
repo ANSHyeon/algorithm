ff327256d123cb422d2ffdea26ac04599e5d45ad
ANSHyeon <127817240+ANSHyeon@users.noreply.github.com>
2023-04-04 16:46:18 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 25.32 ms, Memory: 64 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.kt
@@ -0,0 +1,6 @@
+class Solution {
+    fun solution(my_string: String): String {
+        var answer: String = my_string.toLowerCase().toCharArray().sorted().joinToString("")
+        return answer
+    }
+}
