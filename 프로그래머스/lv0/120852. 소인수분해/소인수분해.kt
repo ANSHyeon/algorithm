843c73e068a31b15c9027719617ced10d3ccd3b2
ANSHyeon <127817240+ANSHyeon@users.noreply.github.com>
2023-04-04 23:19:04 +0900

    [level 0] Title: 소인수분해, Time: 5.90 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.kt
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.kt
@@ -1,29 +1,15 @@
-import kotlin.math.*
-
 class Solution {
     fun solution(n: Int): IntArray {
-        var numList = mutableListOf<Int>()
-        var arr = Array(n+1, {false})
-        arr[0] = true; arr[1] = true;
-        
-        for(i in 2.. Math.sqrt(n+1.toDouble()).toInt()) {
-        if(arr[i] == true) {
-            continue;
+        var numSet = mutableSetOf<Int>()
+        var i = 2
+        var num = n
+        while(num>1){
+            if(num%i==0){
+                numSet.add(i)
+                num = num/i
+            }
+            else {i=i+1}
         }
-
-        for(j in i*i..n step +i) {
-            arr[j] = true;
-        }
-    }
-
-    for(i in 1..n) {
-        if(arr[i] == false) {
-            numList.add(i)
-        }
-    }
-    
-        
-        var answer: IntArray = numList.toIntArray().filter{n%it==0}.toIntArray()
-        return answer
+        return numSet.toIntArray()
     }
 }
