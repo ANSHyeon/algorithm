630c42c549d2941867798628860efbe35bd048c0
ANSHyeon <127817240+ANSHyeon@users.noreply.github.com>
2023-04-04 16:44:21 +0900

    [level 0] Title: 숫자 찾기, Time: 22.22 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.kt
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.kt
@@ -1,5 +1,3 @@
 class Solution {
-    fun solution(num: Int, k: Int) = if(num.toString().toCharArray().count {it.digitToInt() == k}>0)
-    num.toString().toCharArray().indexOf(k.digitToChar())+1
-else -1
+    fun solution(num: Int, k: Int) = ("a"+num.toString()).indexOf(k.toString())
 }
