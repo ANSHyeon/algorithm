5f1be50331b70d40bbe5990a97b1d6c58b136176
ANSHyeon <127817240+ANSHyeon@users.noreply.github.com>
2023-04-11 20:06:36 +0900

    [level 1] Title: 삼총사, Time: 26.17 ms, Memory: 63.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun solution(number: IntArray): Int {
+        var answer: Int = 0
+        for(i in 0..(number.lastIndex-2)){
+            for(j in i+1..(number.lastIndex-1)){
+                for(k in j+1..number.lastIndex){
+                    if(number[i]+number[j]+number[k]==0){
+                        answer++
+                        println("${i} , ${j} , ${k}")
+                    } 
+                }
+            }
+        }
+        return answer
+    }
+}
