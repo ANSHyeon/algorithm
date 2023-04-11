224d6f455cbdc78113824a20ae06d100e3f95dfd
ANSHyeon <127817240+ANSHyeon@users.noreply.github.com>
2023-04-11 20:41:27 +0900

    [level 1] Title: K번째수, Time: 19.92 ms, Memory: 63.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
+        var answer = commands.map{
+            array.copyOfRange(it[0]-1,it[1]).sorted()[it[2]-1]
+        }.toIntArray()
+        return answer
+    }
+}
