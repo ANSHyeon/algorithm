5f554d4cacf0f75a6c3946f8fa928422712f5fb2
ANSHyeon <127817240+ANSHyeon@users.noreply.github.com>
2023-05-18 20:28:46 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 132 ms, Memory: 20760 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val (a,b) = readln().split(" ").map{it.toInt()}
+    when{
+        a>b -> {
+            print(">")
+        }
+        a<b -> {
+            print("<")
+        }
+        else -> {
+            print("==")
+        }
+    }
+}
