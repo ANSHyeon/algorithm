302e0f46eb746733a139d356356a57580de75b7d
ANSHyeon <127817240+ANSHyeon@users.noreply.github.com>
2023-05-18 21:17:51 +0900

    [Bronze II] Title: 음계, Time: 100 ms, Memory: 12308 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.kt
@@ -0,0 +1,9 @@
+fun main(){
+    var read = readln()
+
+    when(read){
+        "1 2 3 4 5 6 7 8" -> print("ascending")
+        "8 7 6 5 4 3 2 1" -> print("descending")
+        else -> print("mixed")
+    }
+}
